488401082f2f1207ee65ccd3c379a93c3161e995
Gevorg Manukyan <gevorg.manukyan@yahoo.com>
2025-02-15 20:37:34 +0400

    remove unused code

--- routes/api.php
+++ routes/api.php
@@ -8,12 +8,6 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:api')->post('/logout', [AuthController::class, 'logout']);
 Route::middleware('auth:api')->get('/user', [AuthController::class, 'user']);
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:api');
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
 
 Route::post('/check', [AuthController::class, 'check']);
 Route::post('/payment', [AuthController::class, 'payment']);
